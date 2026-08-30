1be05f9f895477a9663428d6781cbe655abb5100
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-30 17:52:56 +0700

    Add Fashion Prompt room route

---
+++ app/fashion/page.js
@@ -0,0 +1,10 @@
+import FashionGallery from "@/components/FashionGallery";
+
+export const metadata = {
+  title: "Fashion Prompt | PromptVault",
+  description: "A dedicated fashion infographic prompt room inside PromptVault.",
+};
+
+export default function FashionPage() {
+  return <FashionGallery />;
+}
