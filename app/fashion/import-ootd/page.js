b762446ad0b8b52b3c47842dbaa9d1bfc7f5dac0
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-30 20:13:22 +0700

    Add route for Fashion OOTD importer

---
+++ app/fashion/import-ootd/page.js
@@ -0,0 +1,10 @@
+import FashionOOTDImporter from "@/components/FashionOOTDImporter";
+
+export const metadata = {
+  title: "Fashion OOTD Import | PromptVault",
+  description: "Authenticated OOTD image importer for Fashion Prompt F001–F152.",
+};
+
+export default function FashionOOTDImportPage() {
+  return <FashionOOTDImporter />;
+}
