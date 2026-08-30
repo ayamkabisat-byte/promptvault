57d8b579600f033ed2285a491c9dc8cac6b3de31
ayamkabisat-byte <ayamkabisat@gmail.com>
2026-08-31 00:53:51 +0700

    Add mobile theme tokens

---
+++ mobile/lib/theme.js
@@ -0,0 +1,13 @@
+export const colors = {
+  bg: '#08080b',
+  panel: '#111116',
+  panel2: '#17171e',
+  text: '#f5f3f7',
+  muted: '#9a96a4',
+  dim: '#6f6b78',
+  line: 'rgba(255,255,255,0.09)',
+  orange: '#ff9d2e',
+  lavender: '#b8a1ff',
+  lavender2: '#d8caff',
+  danger: '#ff6d72',
+};
